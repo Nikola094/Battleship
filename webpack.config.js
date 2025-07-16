e11c60537a1a768d5c76d612a65d34a4ff7780f0
Nikola <nikola94.babic@gmail.com>
2025-07-16 12:59:02 +0200

    added webpack, babel and jest alongside folder for javascript

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    static: './dist',
+    port: 3000
+  },
+  mode: 'development'
+};
